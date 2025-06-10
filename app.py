6e856bcb51b27805b837479e976637a5a6e9f1b7
12AM <kimtheng324283@gmail.com>
2025-06-10 22:05:00 +0700

    .
    .

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, request, render_template, send_file
 import yt_dlp
 import os
 import uuid
+import sys
+
+# Add your project directory to the sys.path
+project_home = '/home/Theng/mp3_download'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# Activate your virtualenv
+activate_this = '/home/Theng/mp3_download/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+from app import app as application
 
 app = Flask(__name__)
 DOWNLOAD_DIR = "download"
